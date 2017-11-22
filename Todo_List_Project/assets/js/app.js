b43247985c358550b430b3e0c54612a28b3ef6b9
Aga <a.kolia.grudzien@gmail.com>
2017-11-22 11:22:28 +0100

    Todo list final version

--- Todo_List_Project/assets/js/app.js
+++ Todo_List_Project/assets/js/app.js
@@ -24,6 +24,10 @@ $("input[type='text']").keypress(function(event){
         var todoText = $(this).val();
         $(this).val("");//clearing the input text after adding
         //create a new li and add to ul
-        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 })
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle()
+});
